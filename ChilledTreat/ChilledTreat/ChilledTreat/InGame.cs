956b6dfaaaafedbdce8fddb0ec945dc0f026a7a4
Vegard Strand Lende <lende.vegard@gmail.com>
2012-05-03 14:30:51 +0200

    lagt til InGame klasse

---
+++ ChilledTreat/ChilledTreat/ChilledTreat/InGame.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChilledTreat
+{
+	class InGame : GameState
+	{
+		// Fields
+
+
+		// Constructor
+		public InGame(SpriteBatch spriteBatch, ContentManager content)
+			: base (spriteBatch, content)
+		{
+			// CONTENT LOAD
+		}
+
+		// Methods
+
+		public override void Update()
+		{
+			// LOGIC
+		}
+
+		public override void Draw()
+		{
+			// DRAW THAT SHIT
+		}
+	}
+}
--- ChilledTreat/ChilledTreat/ChilledTreat/Menu.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/Menu.cs
@@ -4,87 +4,66 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace ChilledTreat
 {
-<<<<<<< HEAD
-    class Menu : GameState
-    {
-        
-        SpriteFont menuFont;
-        Color fontColor;
-        string[] menuItems;
-        float[] selectedItem;
-        int menuPos;
-        InputHandler input = InputHandler.Instance;
-
-        public Menu(SpriteBatch spriteBatch, ContentManager content)
-            : base(spriteBatch, content)
-        {
-            // Menu content
-            menuFont = Content.Load<SpriteFont>("fonts/menuFont");
-            fontColor = Color.Aqua;
-            string[] strings = {"New Game", "Levels", "Instructions", "Credits", "EXIT"};
-            menuItems = strings;
-            menuPos = 0;
-            selectedItem = new float[menuItems.Length];
-            for (int i = 0; i < selectedItem.Length; i++)
-            {
-                selectedItem[i] = 1f;
-            }
-            selectedItem[menuPos] = 1.3f;
-        }
-
-        public override void Update()
-        {
-            if (input.IsKeyPressed(input.DownKey))
-            {
-                menuPos++;
-                if (menuPos > selectedItem.Length - 1) menuPos = 0;
-            }
-            if (input.IsKeyPressed(input.UpKey))
-            {
-                menuPos--;
-                if (menuPos < 0) menuPos = selectedItem.Length - 1;
-            }
-            for (int i = 0; i < selectedItem.Length; i++) selectedItem[i] = 1f;
-            selectedItem[menuPos] = 1.3f;
-        }
-        public override void Draw()
-        {
-            int yPos = 100;
-            // Draw here
-
-            for (int i = 0; i < menuItems.Length; i++)
-            {
-                SpriteBatch.DrawString(menuFont, menuItems[i], new Vector2(150, yPos), fontColor, 0, new Vector2(0, 0), selectedItem[i], SpriteEffects.None, 0);
-                yPos += 100;
-            }
-
-
-        }
-=======
 	class Menu : GameState
 	{
-		int _nextState;
+		
+		SpriteFont menuFont;
+		Color fontColor;
+		string[] menuItems;
+		float[] selectedItem;
+		int menuPos;
+		InputHandler input = InputHandler.Instance;
 
-		public Menu(SpriteBatch spriteBatch, ContentManager content, int nextState)
+		public Menu(SpriteBatch spriteBatch, ContentManager content)
 			: base(spriteBatch, content)
 		{
 			// Menu content
-
-			_nextState = nextState;
+			menuFont = Content.Load<SpriteFont>("fonts/menuFont");
+			fontColor = Color.Aqua;
+			string[] strings = {"New Game", "Levels", "Instructions", "Credits", "EXIT"};
+			menuItems = strings;
+			menuPos = 0;
+			selectedItem = new float[menuItems.Length];
+			for (int i = 0; i < selectedItem.Length; i++)
+			{
+				selectedItem[i] = 1f;
+			}
+			selectedItem[menuPos] = 1.3f;
 		}
 
 		public override void Update()
 		{
-			// Logic here
-		   
-		}
+			if (input.IsKeyPressed(input.DownKey))
+			{
+				menuPos++;
+				if (menuPos > selectedItem.Length - 1) menuPos = 0;
+			}
+			if (input.IsKeyPressed(input.UpKey))
+			{
+				menuPos--;
+				if (menuPos < 0) menuPos = selectedItem.Length - 1;
+			}
+			for (int i = 0; i < selectedItem.Length; i++) selectedItem[i] = 1f;
+			selectedItem[menuPos] = 1.3f;
 
+			if (input.IsKeyPressed(input.ActionKey))
+			{
+				if (menuItems[menuPos].Contains("EXIT"))
+				{
+					Game1.Instance.Exit();
+				}
+			}
+		}
 		public override void Draw()
 		{
+			int yPos = 100;
 			// Draw here
 
+			for (int i = 0; i < menuItems.Length; i++)
+			{
+				SpriteBatch.DrawString(menuFont, menuItems[i], new Vector2(150, yPos), fontColor, 0, new Vector2(0, 0), selectedItem[i], SpriteEffects.None, 0);
+				yPos += 100;
+			}
 		}
->>>>>>> 8ddb1cc1e6fb6946fe08ae06accea88f5e2e0bab
-
 	}
 }
