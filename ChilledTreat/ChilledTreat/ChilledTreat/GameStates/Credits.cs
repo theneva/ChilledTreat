45661ef869a32924efb9a581edb4768567283b7e
Simen Bekkhus <beksim10@nith.no>
2012-05-05 14:49:13 +0200

    Renset credits

--- ChilledTreat/ChilledTreat/ChilledTreat/GameStates/Credits.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/GameStates/Credits.cs
@@ -7,34 +7,22 @@ namespace ChilledTreat.GameStates
 	class Credits : GameState
 	{
 		// Fields
-		int nextState;
-		InputHandler input = InputHandler.Instance;
-		FrameInfo _frameInfo = FrameInfo.Instance;
+		readonly InputHandler _input = InputHandler.Instance;
+		readonly FrameInfo _frameInfo = FrameInfo.Instance;
 
-		SpriteFont creditsFont;
-		string[] creditsContent;
-		Color fontColor;
-		int lineBreak = 0;
+		readonly SpriteFont _creditsFont;
+		readonly string[] _creditsContent;
+		readonly Color _fontColor;
+		int _lineBreak = 0;
 		double _currentTime;
 
 		public Credits(SpriteBatch spriteBatch, ContentManager content)
 			: base(spriteBatch, content)
 		{
 			// Credits content
-			creditsFont = content.Load<SpriteFont>("fonts/menuFont");
-			fontColor = Color.Salmon;
-			string[] strings = { "Linje----------------", "Linje----------------", "Linje----------------", "Linje----------------" };
-			creditsContent = strings;
-		}
-
-
-
-
-		// Constructor
-		public Credits(SpriteBatch spriteBatch, ContentManager content, int NextState)
-			: base(spriteBatch, content)
-		{
-			nextState = NextState;
+			_creditsFont = content.Load<SpriteFont>("fonts/menuFont");
+			_fontColor = Color.Salmon;
+			_creditsContent = new[] { "Linje----------------", "Linje----------------", "Linje----------------", "Linje----------------" };
 		}
 
 		// Methods
@@ -42,12 +30,12 @@ namespace ChilledTreat.GameStates
 		public override void Update()
 		{
 			// Logic
-			if (input.IsAbortPressed())
+			if (_input.IsAbortPressed())
 			{
 				Game1.ChangeState(1);
 			}
 
-			lineBreak = 0;
+			_lineBreak = 0;
 
 			_currentTime = _frameInfo.GameTime.TotalGameTime.TotalMilliseconds;
 
@@ -56,10 +44,10 @@ namespace ChilledTreat.GameStates
 		public override void Draw()
 		{
 			// DRAW!!!! LåååL
-			for (int i = 0; i < creditsContent.Length; i++)
+			foreach (string creditEntry in _creditsContent)
 			{
-				lineBreak += 100;
-				SpriteBatch.DrawString(creditsFont, creditsContent[i], new Vector2(360, lineBreak), fontColor);
+				_lineBreak += 100;
+				SpriteBatch.DrawString(_creditsFont, creditEntry, new Vector2(360, _lineBreak), _fontColor);
 			}
 		}
 	}
