f7f7fa540f35204241847c033b30155d77c133db
Martin Lehmann <theneva@hotmail.com>
2012-05-08 12:48:05 +0200

    INFINITE AMMO RAAAAAH aka fjerna litt hardkoding og fiksa litt på shit Simen har gjort med enemyhandler

--- ChilledTreat/ChilledTreat/ChilledTreat/GameStates/InGame.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/GameStates/InGame.cs
@@ -21,32 +21,24 @@ namespace ChilledTreat.GameStates
 			// CONTENT LOAD
 			_player = Player.Instance;
 			Game1.Instance.IsMouseVisible = false;
-
-			EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 1, new Vector2(250, 50));
-			EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 1, new Vector2(350, 150));
-			EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 1, new Vector2(50, 250));
-			EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 1, new Vector2(250, 150));
-			EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 1, new Vector2(350, 250));
-			EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 1, new Vector2(50, 50));
-		
 		}
 
 		// Methods
 
 		// Testing
 		private float _timeSinceLastAdd;
-		private const float EnemiesPerSecond = 4f;
+		private const float EnemiesPerSecond = 1.6f;
 
 		public override void Update()
 		{
 			// LOGIC
-			//_timeSinceLastAdd += Game1.Instance.TargetElapsedTime.Milliseconds;
-			
-			//if (_timeSinceLastAdd >= 1000 / EnemiesPerSecond)
-			//{
-			//    _timeSinceLastAdd -= 1000 / EnemiesPerSecond;
-			//    EnemyHandler.Instance.Add(new Enemy(SpriteBatch, Content, 1, new Vector2(_random.Next(1080), _random.Next(720))));
-			//}
+			_timeSinceLastAdd += Game1.Instance.TargetElapsedTime.Milliseconds;
+
+			if (_timeSinceLastAdd >= 1000 / EnemiesPerSecond)
+			{
+				_timeSinceLastAdd -= 1000 / EnemiesPerSecond;
+				EnemyHandler.Instance.AddEnemy(SpriteBatch, Content, 20);
+			}
 
 			
 
