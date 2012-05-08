c9d2447e8dbd28a67bd63ad8550183eff1f48f7b
Vegard Strand Lende <lende.vegard@gmail.com>
2012-05-08 10:47:41 +0200

    asdf

---
+++ ChilledTreat/ChilledTreat/ChilledTreat/GameStates/GameOver.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+namespace ChilledTreat.GameStates
+{
+	class GameOver : GameState
+	{
+		// FIELDS
+
+		public GameOver(SpriteBatch spriteBatch, ContentManager content)
+			: base(spriteBatch, content)
+		{
+			// LOAD CONTENT
+		}
+
+		public override void Update()
+		{
+			
+		}
+
+		public override void Draw()
+		{
+			
+		}
+	}
+}
