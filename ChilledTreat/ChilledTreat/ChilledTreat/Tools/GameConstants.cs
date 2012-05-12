96ebc41687139e470cd37cd3463cdeccc5e9e909
Simen Bekkhus <beksim10@nith.no>
2012-05-13 01:08:17 +0200

    Fjerna konstanter som ikke er aktuelle

--- ChilledTreat/ChilledTreat/ChilledTreat/Tools/GameConstants.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/Tools/GameConstants.cs
@@ -6,22 +6,9 @@
 		public const bool GodMode = true;
 		
 		public const int PlayerHealth = 100;
-		public const int PlayerDamage = 10000;
 
 		public const int EnemyHealth = 10;
 		public const int EnemyDamage = 30;
-
-
-		// TODO: Simen
-		public static int GunAmmo = 10;
-		public static int GunRoundsPerSecond = 1;
-		public static bool IsGunSplashDamage = false;
-		public static bool IsGunAutomatic = false;
-
-		public const int RifleDefaultAmmo = 20;
-		public const int RifleDefaultRoundsPerSecond = 20;
-		public const bool IsRifleSplashDamage = true;
-		public const bool IsRifleAutomatic = true;
 
 		public const float InitialEnemiesPerSecond = 200;
 		public const int AddEnemyInterval = 5000; // milliseconds
