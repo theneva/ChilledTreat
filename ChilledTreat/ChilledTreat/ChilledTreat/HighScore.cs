2ea79ce4e88c22ad1fca000db56c1793cbd4e285
Simen Bekkhus <beksim10@nith.no>
2012-05-12 17:58:31 +0200

    idk

--- ChilledTreat/ChilledTreat/ChilledTreat/HighScore.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/HighScore.cs
@@ -4,8 +4,6 @@ using System.IO.IsolatedStorage;
 using System.Xml.Serialization;
 using System.IO;
 
-
-
 namespace ChilledTreat
 {
 	public class Highscore
@@ -32,37 +30,41 @@ namespace ChilledTreat
 
 		public Highscore() { }
 
-		static public void SerializeToXml(List<Highscore> highscores)
+		public static void SerializeToXml(List<Highscore> highscores)
 		{
-			XmlSerializer serializer = new XmlSerializer(typeof(List<Highscore>));
+			XmlSerializer serializer = new XmlSerializer(typeof (List<Highscore>));
 #if WINDOWS
 			TextWriter textWriter = new StreamWriter(Game1.Instance.Content.RootDirectory + "/HighScore.xml");
 
 			serializer.Serialize(textWriter, highscores);
 			textWriter.Close();
+
+#elif XBOX
+
+			using (IsolatedStorageFile iso = IsolatedStorageFile.GetUserStoreForApplication())
+			{
+
+				using (
+					IsolatedStorageFileStream stream =
+						new IsolatedStorageFileStream(Game1.Instance.Content.RootDirectory + "/HighScore.xml", FileMode.Create, iso))
+				{
+					serializer.Serialize(stream, highscores);
+
+				}
+			}
 #endif
-			#elif XBOX
-
-using (IsolatedStorageFile iso = IsolatedStorageFile.GetUserStoreForApplication())
-{
-
-using (IsolatedStorageFileStream stream = new IsolatedStorageFileStream(Game1.Instance.Content.RootDirectory + "/HighScore.xml", FileMode.Create, iso))
-{
-	serializer.Serialize(stream, highscores);
-
-}
-#endif
-}
 		}
 
 		public static List<Highscore> DeserializeFromXml()
 		{
-			XmlSerializer deserializer = new XmlSerializer(typeof(List<Highscore>));
+			XmlSerializer deserializer = new XmlSerializer(typeof (List<Highscore>));
 			TextReader textReader = new StreamReader(Game1.Instance.Content.RootDirectory + "/HighScore.xml");
-			var scores = (List<Highscore>)deserializer.Deserialize(textReader);
+			var scores = (List<Highscore>) deserializer.Deserialize(textReader);
 			textReader.Close();
-			
+
 			return scores;
 		}
 	}
+
+
 }
