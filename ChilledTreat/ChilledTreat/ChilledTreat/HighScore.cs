b9fea6fa71874d126308f277f71e0ac066cb6c09
Simen Bekkhus <beksim10@nith.no>
2012-05-12 20:46:30 +0200

    Kommenterte highscore

--- ChilledTreat/ChilledTreat/ChilledTreat/HighScore.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/HighScore.cs
@@ -11,10 +11,13 @@ namespace ChilledTreat
 {
 	public class Highscore
 	{
+		#region Fields
 		public string Name { get; set; }
 		public int Score { get; set; }
 		public DateTime CurrentTime { get; set; }
+		#endregion
 
+		#region Constructors
 		/// <param name="name">The name of the Player</param>
 		/// <param name="score">The score of the Player</param>
 		public Highscore(String name, int score)
@@ -24,6 +27,11 @@ namespace ChilledTreat
 			CurrentTime = DateTime.Now;
 		}
 
+		//An empty constructor has to be used for the XML stuff
+		public Highscore() { }
+		#endregion
+
+		#region Methods
 		public static List<Highscore> CreateHighScore()
 		{
 			List<Highscore> highScoreList = DeserializeFromXml();
@@ -31,40 +39,39 @@ namespace ChilledTreat
 			return highScoreList;
 		}
 
-		public Highscore() { }
-
+		//This method serializes the list, and prints it to a file, so that it can be accessed in another session
 		public static void SerializeToXml(List<Highscore> highscores)
 		{
 			XmlSerializer serializer = new XmlSerializer(typeof (List<Highscore>));
 #if WINDOWS
-			TextWriter textWriter = new StreamWriter(Game1.Instance.Content.RootDirectory + "/HighScore.xml");
-
+			TextWriter writer = new StreamWriter(Game1.Instance.Content.RootDirectory + "/HighScore.xml");
 #elif XBOX
-
+			//This shit doesn't work
 			using (IsolatedStorageFile iso = IsolatedStorageFile.GetUserStoreForApplication())
 			{
 				using (
-					IsolatedStorageFileStream textWriter =
+					IsolatedStorageFileStream writer =
 						new IsolatedStorageFileStream(Game1.Instance.Content.RootDirectory + "/HighScore.xml", FileMode.Create, iso))
 				{
 #endif
-					serializer.Serialize(textWriter, highscores);
-					textWriter.Close();
+			serializer.Serialize(writer, highscores);
+			writer.Close();
 #if XBOX
-
 				}
 			}
 #endif
 		}
 
+		//This method deserializes the XML file, and returns it in an array
 		private static List<Highscore> DeserializeFromXml()
 		{
 			XmlSerializer deserializer = new XmlSerializer(typeof (List<Highscore>));
 			TextReader textReader = new StreamReader(Game1.Instance.Content.RootDirectory + "/HighScore.xml");
-			var scores = (List<Highscore>) deserializer.Deserialize(textReader);
+			List<Highscore> scores = (List<Highscore>) deserializer.Deserialize(textReader);
 			textReader.Close();
 
 			return scores;
 		}
+		#endregion
 	}
 }
