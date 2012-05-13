77c779c8c10c1fb47b35c418f6849ad581e87af7
Simen Bekkhus <beksim10@nith.no>
2012-05-13 23:19:06 +0200

    credits scaled

--- ChilledTreat/ChilledTreat/ChilledTreat/GameStates/Credits.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/GameStates/Credits.cs
@@ -7,30 +7,25 @@ namespace ChilledTreat.GameStates
 	class Credits : GameState
 	{
 		//fields
-		readonly SpriteFont _creditsFont;
-		readonly SpriteFont _creditsFontSources;
-		readonly string[] _creditsContent;
-		readonly string[] _creditsContentSources;
-		private int _shift;
-		private bool _startAnew = true;
 		readonly InputHandler _input = InputHandler.Instance;
 		readonly FrameInfo _frameInfo = FrameInfo.Instance;
+
+		readonly SpriteFont _creditsFont, _creditsFontSources;
+		readonly string[] _creditsContent, _creditsContentSources;
+		private int _shift;
+		private bool _startAnew = true, _drawLogos, _songstart;
 		private double _startCreditsTimer, _currentTime;
 		readonly Texture2D _xnaLogo, _nithLogo;
-		bool _drawLogos, _songstart;
 		readonly Song _creditMusic;
 
 		public Credits()
 		{
 			// Credits content
-			_creditMusic = Game1.Instance.Content.Load<Song>("Music/DubMood");
-			MediaPlayer.IsRepeating = true;
 			_xnaLogo = Game1.Instance.Content.Load<Texture2D>("Images/xnaLogo");
 			_nithLogo = Game1.Instance.Content.Load<Texture2D>("Images/nithLogo");
 			_creditsFont = Game1.Instance.Content.Load<SpriteFont>("Fonts/CreditsFont");
 			_creditsFontSources = Game1.Instance.Content.Load<SpriteFont>("Fonts/urlFont");
 			_creditsContent = new[] {
-
 				//People who have worked on this project
 				".....CREDITS.....",
 				"Programmers",
@@ -51,7 +46,6 @@ namespace ChilledTreat.GameStates
 				" "
 			};
 
-
 			//sourcepaths to some of the content used in this project.
 			_creditsContentSources = new[] {
 				"blogcdn.com/joystiq.com/media/2006/06/reticule.jpg",
@@ -70,13 +64,18 @@ namespace ChilledTreat.GameStates
 				"i.istockimg.com/file_thumbview_approve/1096341/2/stock-photo-1096341-bullet-chain.jpg",
 				"wiki.teamfortress.com/w/images/0/06/Minigun_1st_person.png"
 			};
+
+
+			_creditMusic = Game1.Instance.Content.Load<Song>("Music/DubMood");
+			MediaPlayer.IsRepeating = true;
 		}
 
 		// Methods
 
 		public override void Update()
 		{
-			// Logic
+			_currentTime = _frameInfo.GameTime.TotalGameTime.TotalSeconds - _startCreditsTimer;
+
 			_shift = 0;
 
 			if (_startAnew)
@@ -92,7 +91,6 @@ namespace ChilledTreat.GameStates
 				_startAnew = true;
 				MediaPlayer.Stop();
 			}
-			_currentTime = _frameInfo.GameTime.TotalGameTime.TotalSeconds - _startCreditsTimer;
 
 			if (!_songstart)
 			{
@@ -109,24 +107,20 @@ namespace ChilledTreat.GameStates
 
 		public override void Draw()
 		{
-			// DRAW!!!! LåååL
-			
 			foreach (string creditEntry in _creditsContent)
 			{
 				//draws people involved in credits
-				_shift++;
-				Game1.Instance.SpriteBatch.DrawString(_creditsFont, creditEntry, new Vector2((Game1.GameScreenWidth / 6) - (creditEntry.Length / 2), 680 + (_shift * 50) - (60 * (float)_currentTime)), Color.White);
+				Game1.Instance.SpriteBatch.DrawString(_creditsFont, creditEntry, new Vector2((Game1.GameScreenWidth / 6) - (creditEntry.Length / 2), 680 * Game1.GameScale + (++_shift * 50) - (60 * (float)_currentTime)), Color.White, 0, Vector2.Zero, Game1.GameScale, SpriteEffects.None, 0);
 			}
 			foreach (string creditSourcesEntry in _creditsContentSources)
 			{
 				//draws sources used in project
-				_shift++;
-				Game1.Instance.SpriteBatch.DrawString(_creditsFontSources, creditSourcesEntry, new Vector2(Game1.GameScreenWidth / 6, 680 + (_shift * 50) - (60 * (float)_currentTime)), Color.White);
+				Game1.Instance.SpriteBatch.DrawString(_creditsFontSources, creditSourcesEntry, new Vector2(Game1.GameScreenWidth / 6, 680 * Game1.GameScale + (++_shift * 50) - (60 * (float)_currentTime)), Color.White, 0, Vector2.Zero, Game1.GameScale, SpriteEffects.None, 0);
 			}
 			if (!_drawLogos) return;
 				//draws logos at the end of creditsscreen
-			Game1.Instance.SpriteBatch.Draw(_nithLogo, new Vector2((Game1.GameScreenWidth - _nithLogo.Width) / 2f, (Game1.GameScreenHeight - _nithLogo.Height) / 2.5f), Color.White);
-			Game1.Instance.SpriteBatch.Draw(_xnaLogo, new Vector2((Game1.GameScreenWidth - _xnaLogo.Width) / 2f, (Game1.GameScreenHeight - _xnaLogo.Height) / 1.25f), Color.White);
+			Game1.Instance.SpriteBatch.Draw(_nithLogo, new Vector2((Game1.GameScreenWidth - _nithLogo.Width * Game1.GameScale) / 2f, (Game1.GameScreenHeight - _nithLogo.Height * Game1.GameScale) / 2.5f), _nithLogo.Bounds, Color.White, 0, Vector2.Zero, Game1.GameScale, SpriteEffects.None, 0);
+			Game1.Instance.SpriteBatch.Draw(_xnaLogo, new Vector2((Game1.GameScreenWidth - _xnaLogo.Width * Game1.GameScale) / 2f, (Game1.GameScreenHeight - _xnaLogo.Height * Game1.GameScale) / 1.25f), _xnaLogo.Bounds, Color.White, 0, Vector2.Zero, Game1.GameScale, SpriteEffects.None, 0);
 		}
 	}
 }
