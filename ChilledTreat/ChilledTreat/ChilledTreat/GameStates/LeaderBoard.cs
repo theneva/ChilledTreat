71375131606f567391441a006c7db7f9086cb917
Vegard Strand Lende <lende.vegard@gmail.com>
2012-05-13 17:16:54 +0200

    asdf

--- ChilledTreat/ChilledTreat/ChilledTreat/GameStates/LeaderBoard.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/GameStates/LeaderBoard.cs
@@ -63,8 +63,7 @@ namespace ChilledTreat.GameStates
 			Game1.Instance.SpriteBatch.DrawString(_menuFont, "Leaderboard", new Vector2(Game1.GameScreenWidth / 3f - 50, 50), Color.White);
 
 
-			Game1.Instance.SpriteBatch.Draw(_lbTexture, new Rectangle(0, 640, 1280, 80), Color.Black);
-			Game1.Instance.SpriteBatch.DrawString(_menuFont, "Leaderboard", new Vector2(Game1.GameScreenWidth / 3f - 100, 50), Color.White);
+		
 
 		}
 	}
