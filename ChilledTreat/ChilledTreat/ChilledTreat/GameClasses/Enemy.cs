f81fd89166f1c01b91b357dd7b7496bee794900d
Martin Lehmann <theneva@hotmail.com>
2012-05-13 23:00:25 +0200

    couple comments

--- ChilledTreat/ChilledTreat/ChilledTreat/GameClasses/Enemy.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/GameClasses/Enemy.cs
@@ -52,10 +52,10 @@ namespace ChilledTreat.GameClasses
 			: this(isHealth ? value : GameConstants.EnemyHealth, isHealth ? GameConstants.EnemyHealth : value) { }
 
 		/// <summary>
-		/// Main constructor. Gives each field its value.
+		/// Main constructor. Assigns each field.
 		/// </summary>
-		/// <param name="health">Value of health</param>
-		/// <param name="damage">Value of damage</param>
+		/// <param name="health">Max health of enemy</param>
+		/// <param name="damage">Max damage the enemy can deal</param>
 		public Enemy(int health, int damage)
 		{
 			Alive = true;
@@ -72,9 +72,10 @@ namespace ChilledTreat.GameClasses
 			// Redundant but not sufficiently inefficient for a workaround
 			Scale = 0.008f * (_position.Y);
 
-			_position = new Vector2(538 + _random.Next(192) - _frameSize.X * Scale,
-				Game1.GameScreenHeight - 480 - _frameSize.Y * Scale);
-
+			// 510 is the height of the road, 192 is the width of the top of the road,
+			// 540 is the distance from the left border to the top of the road
+			_position = new Vector2(510 + _random.Next(192) - _frameSize.X * Scale,
+				Game1.GameScreenHeight - 540 - _frameSize.Y * Scale);
 
 			_muzzleFlare = Game1.Instance.Content.Load<Texture2D>("Images/usableMuzzleFlare");
 			_drawMuzzleFlare = false;
@@ -120,11 +121,13 @@ namespace ChilledTreat.GameClasses
 		{
 			Alive = false;
 
+			// Play a random sound
 			EnemyHandler.Instance.SoundEffects[InGame.Random.Next(EnemyHandler.Instance.SoundEffects.Count)].Play();
 			Player.Instance.SuccesfullKill();
 
 			_currentFrame = new Point(0, 1);
 			_currentFrameOrigin = _frameSize.Y;
+			// Width, height of dead enemies - could use some improvement
 			_frameSize = new Point(78, 60);
 			_sheetSize = new Point(3, 1);
 		}
