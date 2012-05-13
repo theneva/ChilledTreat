03c199cc9b79100bc5c074391d4ec409674434f7
Martin Lehmann <theneva@hotmail.com>
2012-05-13 21:40:11 +0200

    Oppdaterte dokumentasjon, satt GodMode tilbake til False, endret EnemyDamage til 15, og baserer InitialEnemiesPerSecond på GodMode (2 uten, 20 med)

--- ChilledTreat/ChilledTreat/ChilledTreat/Tools/GameConstants.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/Tools/GameConstants.cs
@@ -2,14 +2,14 @@ namespace ChilledTreat.Tools
 {
 	static class GameConstants
 	{
-		public const bool GodMode = true;
+		public const bool GodMode = false;
 		
 		public const int PlayerHealth = 100;
 
 		public const int EnemyHealth = 10;
-		public const int EnemyDamage = 30;
+		public const int EnemyDamage = 15;
 
-		public const float InitialEnemiesPerSecond = 2f;
+		public const float InitialEnemiesPerSecond = GodMode ? 20f : 2f;
 		public const int AddEnemyInterval = 5000; // milliseconds
 	}
 }
