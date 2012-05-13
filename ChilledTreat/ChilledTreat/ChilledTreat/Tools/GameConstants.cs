fb6a75b58b8eca8e4317820779e51ec6a770f603
Vegard Strand Lende <lende.vegard@gmail.com>
2012-05-13 14:07:18 +0200

    masse godmods :S

--- ChilledTreat/ChilledTreat/ChilledTreat/Tools/GameConstants.cs
+++ ChilledTreat/ChilledTreat/ChilledTreat/Tools/GameConstants.cs
@@ -3,14 +3,14 @@
 	static class GameConstants
 	{
 
-		public const bool GodMode = true;
+		public const bool GodMode = false;
 		
 		public const int PlayerHealth = 100;
 
 		public const int EnemyHealth = 10;
 		public const int EnemyDamage = 30;
 
-		public const float InitialEnemiesPerSecond = 200;
+		public const float InitialEnemiesPerSecond = 2;
 		public const int AddEnemyInterval = 5000; // milliseconds
 	}
 }
